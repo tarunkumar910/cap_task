a3830e9f7b1670bc505ad9d47987ac4eec2c2d61
tarunkumar910 <deathkiller447@gmail.com>
2026-02-23 10:46:23 +0530

    Add day21-02-2026 work: day_21_02_2026.ipynb, question1_for_21_02_2026.py

---
+++ question1_for_21_02_2026.py
@@ -0,0 +1,19 @@
+# WAP for Day 21
+
+import logging
+
+logging.basicConfig(
+    filename='day21.log',
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def main():
+    """
+    Main function for Day 21
+    """
+    print("Day 21 setup complete.")
+    logging.info("Day 21 setup complete.")
+
+if __name__ == "__main__":
+    main()
